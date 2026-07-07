d0336d17be60015ad92bbc71c5f70feeb813dd9f
uddisharma <uddibhardwaj08@gmail.com>
2026-07-07 22:27:33 +0530

    refactor: update server configuration and request handling logic in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const { connMongoose } = require('./config/db');
-const connectCloudinary = require('./config/cloudinary'); 
+const connectCloudinary = require('./config/cloudinary');
 const userRouter = require('./Router/userRouter');
 const productRouter = require('./Router/productRouter');
 const cartRouter = require('./Router/cartRouter');
@@ -12,7 +12,7 @@ const orderRouter = require('./Router/orderRouter');
 const app = express();
 
 // ─────────────── Middleware ───────────────
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
@@ -25,13 +25,13 @@ app.get('/', (req, res) => {
   res.send('Server is running...');
 });
 
-app.use('/api/user',userRouter);
-app.use('/api/product',productRouter);
-app.use('/api/cart',cartRouter);
-app.use('/api/order',orderRouter);
+app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
 
 // ─────────────── Start Server ───────────────
-const HOST_NAME = process.env.HOST_NAME || '127.0.0.1';
+const HOST_NAME = "0.0.0.0"
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, HOST_NAME, () => {
